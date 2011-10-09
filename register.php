9709456e7cb8e942c662e551b21f930ceca595ac
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 15:36:13 +0100

    added more stuff

--- register.php
+++ register.php
@@ -1,11 +1,20 @@
 <?php
-
 // Register Page
 
 include 'includes/header.php';
 ?>
 <h1>Register</h1>
-<?php
+<form>
+    <table>
+        <tr>
+            <td>Firstname: <input type="textbox" name="firstname">Firstname</input> </td>
+            <td>Surname: <input type="textbox" name="surname">Surname</input></td>
+            <td>Submit:<button type="submit">Submit</button></td>
+        </tr>
+    </table>
 
+
+</form>
+<?php
 include 'includes/footer.php';
 ?>
