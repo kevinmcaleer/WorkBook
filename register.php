023afc4b6b0e711c9f44c06ea06c2e825bf3ad4d
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 15:37:34 +0100

    added more stuff

--- register.php
+++ register.php
@@ -6,11 +6,9 @@ include 'includes/header.php';
 <h1>Register</h1>
 <form>
     <table>
-        <tr>
-            <td>Firstname: <input type="textbox" name="firstname">Firstname</input> </td>
-            <td>Surname: <input type="textbox" name="surname">Surname</input></td>
-            <td>Submit:<button type="submit">Submit</button></td>
-        </tr>
+        <tr><td>Firstname: <input type="textbox" name="firstname">Firstname</input> </td></tr>
+        <tr><td>Surname: <input type="textbox" name="surname">Surname</input></td></tr>
+        <tr><td>Submit:<button type="submit">Submit</button></td></tr>
     </table>
 
 
