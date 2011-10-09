1305ade0ef20c5799eee7693d50f6e84d1cdc95f
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 19:33:35 +0100

    added more stuff

--- classes/classPassword.php
+++ classes/classPassword.php
@@ -14,7 +14,6 @@ class Password {
     const alpha_lower_include = 1;
     const number = 2;
     const symbol = 3;
-    
 
     public function generatePassword($p_len) {
 
@@ -26,62 +25,34 @@ class Password {
         echo 'Alpha_lower_include is:', $this->alpha_lower_include, '<br />';
         echo 'Number_include is: ', $this->number_include, '<br />';
         echo 'Symbol_Include is: ', $this->symbol_include, '<br /><br />';
-        
+
         $this->password = "xxxxxxxx";
         $this->length = $p_len;
         //$this->password[0] = $this->length;
         $n = 0;
         while ($n < $this->length) {
-            
+
             echo 'N is: ', $n, ' Password is:', $this->password, ' <br />';
 
 // choose a character type
             $dice = rand(0, 3); // roll the dice
-            
             // check if the dice value was valid, otherwise roll again
-            if($dice)
-            
-            echo 'Dice Roll Result was: ', $dice, ' <br/> ';
-            switch ($dice) {
-                case alpha_upper_include: { // alpha_upper
-                        echo 'Its an alpha Upper Case';
-                        if ($this->alpha_upper_include) {
+            if ($dice)
+                echo 'Dice Roll Result was: ', $dice, ' <br/> ';
 
-                            $txt = chr(rand(1, 26));
-                            echo $txt;
-                            $this->password[$n] = strtoupper($txt);
-                        } else {
-                            echo 'alpha_upper_include must have been false <br />';
-                        }
-
-                        break;
-                    }
-                case alpha_lower_include: { // alpha_lower
-                        echo 'Its a Lower Case Alpha';
-                        if ($this->alpha_lower_include) {
-                            $txt = chr(rand(1, 26));
-                            echo $txt;
-                            $this->password[$n] = strtolower($txt);
-                        }
-                        break;
-                    }
-                case number:{// number
-                    echo 'Its a Number: ';
-                    if ($this->number_include) {
-                        $this->password[$n] = rand(0, 9);
-                    }
-
-                    break;}
-                case symbol:{// symbol
-                    echo 'Its a Symbol: ';
-                    if ($this->symbol_include) {
-                        // TODO include more symbols to chose from!
-                        $this->password[$n] = '!';
-                    }
-
-                    break;}
-            } // end switch
-            $n++;
+            if ($dice == alpha_upper_include) {
+                $txt = chr(rand(1, 26));
+                echo $txt;
+                $this->password[$n] = strtoupper($txt);
+            } elseif ($dice == alpha_lower_include) {
+                $txt = chr(rand(1, 26));
+                echo $txt;
+                $this->password[$n] = strtolower($txt);
+            } elseif ($dice == number) {
+                $this->password[$n] = rand(0, 9);
+            } elseif ($dice == symbol) {
+                $this->password[$n] = '!';
+            }
         } // end while
         return $this->password;
     }
