5d784522589801520450cf867ac06ecb1a3f3f71
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 16:11:44 +0100

    added more stuff

--- register.php
+++ register.php
@@ -6,8 +6,16 @@ include 'includes/header.php';
 <?php
 if (isset($_POST['submitted'])) {
 
-// TODO check if the email address is already used
-// if not then add user
+// check if the email address is already used
+
+    include 'classes/classContact.php';     // include the contact class
+    $newUser = new Contact();               // create a new contact
+    if ($newUser->checkExists($_POST['email'])) {
+        echo 'Sorry that email address is already registered.';
+    } else {
+        // if not then add user
+        
+    }
     ?>
 
 
@@ -23,6 +31,7 @@ if (isset($_POST['submitted'])) {
         <table>
             <tr><td>Firstname: <input type="text" name="firstname">Firstname</input> </td></tr>
             <tr><td>Surname: <input type="text" name="surname">Surname</input></td></tr>
+            <tr><td>Email Address: <input type="text" name="email">Email</input></td></tr>
             <tr><td><input type="hidden" name="submitted" value="login"></input>
             <tr><td>Submit<input type="submit" value="true"></td></tr>
         </table>
