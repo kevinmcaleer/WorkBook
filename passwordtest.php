d65ab5d5473b01dab910763d6a7839470198afe3
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 20:42:13 +0100

    added more stuff

--- passwordtest.php
+++ passwordtest.php
@@ -4,6 +4,7 @@ include 'classes/classPassword.php';
 $myPass = new Password();
 $myPass->symbol_include = FALSE;
 $myPass->number_include = FALSE;
+$myPass->alpha_upper_include = FALSE;
 $test = $myPass->generatePassword(8);
 echo $test;
 ?>
