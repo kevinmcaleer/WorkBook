d03294f04e74da8d69b06e286f9a4dafb39c01af
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 15:11:30 +0100

    changed contacts page

---
+++ privacy_policy.php
@@ -0,0 +1,14 @@
+<?php
+// @file privacy_policy.php
+// @project Princify
+// @author Kevin McAleer
+// @version 1.0
+// @description Displays the Privacy Policy
+
+include '/includes/header.php';
+?>
+<h1>Privacy Policy</h1>
+
+<?php
+include '/includes/footer.php';
+?>
