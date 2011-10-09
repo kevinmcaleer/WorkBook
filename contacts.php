5adbaf7c0f13bcb399efddadfbf4db91f1cd394d
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 14:24:56 +0100

    changed contacts page

--- contacts.php
+++ contacts.php
@@ -7,7 +7,7 @@
     <h1> contacts list</h1>
 
     <?php
-    include_once 'classes/classContact.php';
+    include_once 'classes/classContactView.php';
     $myContactsList = new classContactView;
     $myContactsList->show(1); // TODO change this to be the currently logged in user
     ?>
