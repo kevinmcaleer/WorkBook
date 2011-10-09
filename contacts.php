6cf2b3e9882d3176239d390a509e89a64b957489
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 14:20:30 +0100

    added contacts

---
+++ contacts.php
@@ -0,0 +1,15 @@
+<?php
+// This is the contacts page
+// @author Kevin McAleer
+// @version 1.0
+?>
+<div class="contacts list">
+    <h1> contacts list</h1>
+
+    <?php
+    include_once 'includes/classContactClass.php';
+    $myContactsList = new classContactsView;
+    $myContactsList->show(1); // TODO change this to be the currently logged in user
+    ?>
+
+</div>
