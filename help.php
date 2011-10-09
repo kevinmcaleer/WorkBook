fa9feefbfd8eafe0197be5e308be30b3d79811c7
Kevin McAleer <kevinmcaleer@me.com>
2011-10-09 15:27:04 +0100

    added more stuff

---
+++ help.php
@@ -0,0 +1,11 @@
+<?php
+
+// help.php
+
+include 'includes/header.php';
+
+?>
+<h1>Help & Support</h1>
+<?php
+include 'includes/footer.php';
+?>
--- includes/nav.php
+++ includes/nav.php
@@ -2,5 +2,5 @@
 // nav.php
 ?>
 <div class="nav">
-    <a href="index.php">Home</a> | Profile | Help 
+    <a href="index.php">Home</a> | <a href="profile.php">Profile</a> | <a href="help.php">Help</a> 
 </div>
